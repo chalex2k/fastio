da12dbb9849fd6332bb2a3872ff727da6366fd96
chalex2k <chalex2k@gmail.com>
2025-06-28 23:08:17 +0300

    Print single variables

--- fastio.go
+++ fastio.go
@@ -1,12 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	print(1)
 }
 
+//nolint:gochecknoglobals
+var outputStream io.Writer
+
+func initStdStreams() {
+	outputStream = os.Stdout
+}
+
+func init() {
+	initStdStreams()
+}
+
 //nolint:revive,predeclared
 func print(a ...any) {
-	fmt.Println(a...)
+	for _, v := range a {
+		switch val := v.(type) {
+		case rune:
+			_, err := fmt.Fprintf(outputStream, "%c", val)
+			if err != nil {
+				panic(err)
+			}
+		default:
+			_, err := fmt.Fprint(outputStream, val)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	_, err := fmt.Fprintln(outputStream) // ln
+	if err != nil {
+		panic(err)
+	}
 }
--- fastio_test.go
+++ fastio_test.go
@@ -1,6 +1,38 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestPrint(_ *testing.T) {
+func TestPrint(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    any
+		expected string
+	}{
+		{"Positive number", 42, "42\n"},
+		{"Zero", 0, "0\n"},
+		{"Negative number", -3, "-3\n"},
+		{"String", "line", "line\n"},
+		{"Empty line", "", "\n"},
+		{"Char", 'A', "A\n"},
+		{"True", true, "true\n"},
+		{"False", false, "false\n"},
+		{"Uint64", uint64(30_000_000_000), "30000000000\n"},
+		{"Float number", 1.333555, "1.333555\n"},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			out := new(bytes.Buffer)
+			outputStream = out
+
+			print(testCase.input)
+
+			if out.String() != testCase.expected {
+				t.Errorf("print(%d) = %q, want %q", testCase.input, out, testCase.expected)
+			}
+		})
+	}
 }
